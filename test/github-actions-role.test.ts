dc64720b40cf6c2ecbc2d11839c7e3180013cffa
Anderson <andersong.9012@gmail.com>
2022-05-05 15:51:56 -0500

    Initial commit

---
+++ test/github-actions-role.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GithubActionsRole from '../lib/github-actions-role-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/github-actions-role-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GithubActionsRole.GithubActionsRoleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
